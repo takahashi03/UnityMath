c1f37b5dce951a96ea3ca5d3fc8542308911b4ec
TakahashiChisato <takahashi.k017a1654@gmail.com>
2019-07-11 18:53:46 +0900

    ファイルの整理、初期ファイルの削除

---
+++ UnityMathProject/Assets/Scripts/BinaryDecimalConverter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BinaryDecimalConverter : MonoBehaviour
+{
+    [SerializeField]
+    private InputField inputBinary;    
+    private int binary;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void InputBinary()
+    {
+        binary = int.Parse(inputBinary.text);
+        Debug.Log(binary);
+    }
+
+    private void InputBinaryException()
+    {
+
+    }
+}
